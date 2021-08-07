8a8c473d35f769e144b00def005e71e5da55ac80
Dmitrii <dpushk@gmail.com>
2021-08-08 00:36:22 +0300

    JS дополнение

---
+++ app.js
@@ -0,0 +1,8 @@
+const board = document.querySelector ('#board')
+const SQUARES_NUMBER = 450
+
+for (let i = 0; i<SQUARES_NUMBER; i++) {
+    const square = document.createElement ('div')
+    square.classList.add ('square')
+    board.append (square)
+}
